08a485f01e66d8fbe4701bfd35bccf7e863d86d5
GretaSabaliauskiene <63239556+GretaSabaliauskiene@users.noreply.github.com>
2020-05-17 12:10:55 +0300

    Add files via upload

--- Mosh/exercise.js
+++ Mosh/exercise.js
@@ -120,3 +120,22 @@ function showStars(rows) {
 for(let i=1; i<=rows; i++)
     console.log(stars += '*');
 }
+
+// showPrimes
+
+showPrimes(20);
+
+function showPrimes(limit) {
+for(let number = 2; number <= limit; number++){
+
+    let isPrime = true;
+   for(let factor = 2; factor < number; factor++){
+        if(number % factor === 0){
+        isPrime = false;
+        break;  
+        }
+    }   
+
+    if(isPrime) console.log(number);
+    }  
+}
